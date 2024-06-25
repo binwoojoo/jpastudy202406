fc1c0b6dfa1a294602923cc2f05e8bd8ca83839a
oooowb <oooo940212@gmail.com>
2024-06-25 12:15:01 +0900

    spring jpa entity 생성

---
+++ src/test/java/com/spring/jpastudy/chap02/repository/StudentRepositoryTest.java
@@ -0,0 +1,72 @@
+package com.spring.jpastudy.chap02.repository;
+
+import com.spring.jpastudy.chap02.entity.Student;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback
+class StudentRepositoryTest {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @BeforeEach
+    void insertData() {
+        Student s1 = Student.builder()
+                .name("쿠로미")
+                .city("청양군")
+                .major("경제학")
+                .build();
+
+        Student s2 = Student.builder()
+                .name("춘식이")
+                .city("서울시")
+                .major("컴퓨터공학")
+                .build();
+
+        Student s3 = Student.builder()
+                .name("어피치")
+                .city("제주도")
+                .major("화학공학")
+                .build();
+
+        studentRepository.save(s1);
+        studentRepository.save(s2);
+        studentRepository.save(s3);
+    }
+
+    @Test
+    @DisplayName("dummy test")
+    void dummy() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    @DisplayName("이름이 춘식이인 학생의 모든 정보를 조회한다")
+    void findByNameTest() {
+        //given
+        String name = "춘식이";
+        //when
+        List<Student> byName = studentRepository.findByName(name);
+        //then
+        assertEquals(1, byName.size());
+
+        System.out.println("춘식이@@@@@@@@@" + byName.get(0));
+    }
+
+}
