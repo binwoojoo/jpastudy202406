0b4e9ccd5d2cd6380470cff1fa775d63d7eaab8c
oooowb <oooo940212@gmail.com>
2024-06-28 10:19:24 +0900

    Custom RepositoryImp

--- src/main/java/com/spring/jpastudy/chap06_querydsl/repository/IdolRepository.java
+++ src/main/java/com/spring/jpastudy/chap06_querydsl/repository/IdolRepository.java
@@ -3,5 +3,5 @@ package com.spring.jpastudy.chap06_querydsl.repository;
 import com.spring.jpastudy.chap06_querydsl.entity.Idol;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IdolRepository extends JpaRepository<Idol,Long> {
+public interface IdolRepository extends JpaRepository<Idol, Long>, IdolCustomRepository {
 }
---
+++ src/main/java/com/spring/jpastudy/chap06_querydsl/service/IdolService.java
@@ -0,0 +1,35 @@
+package com.spring.jpastudy.chap06_querydsl.service;
+
+import com.spring.jpastudy.chap06_querydsl.entity.Idol;
+import com.spring.jpastudy.chap06_querydsl.repository.IdolRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional // JPA , QueryDsl 을 쓸때는 붙여야 함
+public class IdolService {
+
+    private final IdolRepository idolRepository;
+
+    // 아이돌을 나이 순으로 내림차 정렬해서 조회
+    public List<Idol> getIdols() {
+        // <1번째 방법>
+//        List<Idol> idolList = idolRepository.findAll();
+//        return idolList.stream()
+//                .sorted(Comparator.comparing(Idol::getAge).reversed())
+//                .collect(Collectors.toList());
+
+        // <2번째 방법>
+//        List<Idol> idolList = idolRepository.findAllBySorted();
+//        return idolList;
+
+        List<Idol> idolList = idolRepository.foundByGroupName();
+        return idolList;
+    }
+
+}
