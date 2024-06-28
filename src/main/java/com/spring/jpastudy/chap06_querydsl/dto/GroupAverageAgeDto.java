99aa0dd93370fcbc4ca5dda3dfa134f5511f03e1
oooowb <oooo940212@gmail.com>
2024-06-28 11:47:52 +0900

    Dto 사용법

---
+++ src/main/java/com/spring/jpastudy/chap06_querydsl/dto/GroupAverageAgeDto.java
@@ -0,0 +1,18 @@
+package com.spring.jpastudy.chap06_querydsl.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GroupAverageAgeDto {
+
+    private String groupName;
+    private double averageAge;
+
+}
